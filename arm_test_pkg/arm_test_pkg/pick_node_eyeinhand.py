d7500fbc071c9a5c087785991dd0194737d30411
goong-1 <sin86459020kr@gmail.com>
2026-07-03 19:42:58 +0900

    Update pick_node_eyeinhand.py

--- arm_test_pkg/arm_test_pkg/pick_node_eyeinhand.py
+++ arm_test_pkg/arm_test_pkg/pick_node_eyeinhand.py
@@ -444,8 +444,8 @@ class PickNode(Node):
                 # 2층 (기존)
                 if y > 0:
                     y -= y * 0.13
-                elif y < 0:
-                    y += y * 0.02
+                # elif y < 0:
+                #     y += y * 0.0
                   
             self.get_logger().info(
                 f"피킹 좌표(보정 후): x={x:.1f}, y={y:.1f}, z={z:.1f}, "
