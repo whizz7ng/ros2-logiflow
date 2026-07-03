9becdcd12d5ef336b26d10611b27de25047f9972
goong-1 <sin86459020kr@gmail.com>
2026-07-03 19:08:23 +0900

    Update pick_node_eyeinhand.py

--- arm_test_pkg/arm_test_pkg/pick_node_eyeinhand.py
+++ arm_test_pkg/arm_test_pkg/pick_node_eyeinhand.py
@@ -443,7 +443,7 @@ class PickNode(Node):
             else:
                 # 2층 (기존)
                 if y > 0:
-                    y -= y * 0.14
+                    y -= y * 0.15
                 elif y < 0:
                     y += y * 0.03
                   
