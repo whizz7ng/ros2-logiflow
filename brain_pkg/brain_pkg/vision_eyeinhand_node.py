f6b5ecec4bb296269dcadaadc82ba7a894a0180a
goong-1 <sin86459020kr@gmail.com>
2026-07-10 01:02:30 +0900

    Update PLACE_OFFSET_X to -150.0

--- brain_pkg/brain_pkg/vision_eyeinhand_node.py
+++ brain_pkg/brain_pkg/vision_eyeinhand_node.py
@@ -66,7 +66,7 @@ MODE_QR_PLACE = 'qr_place'
 MODE_MARKER_ALIGN = 'marker_align'   # [신규] 블록 검출 전 마커 정면(yaw) 정렬 전용 모드
 
 # place 오프셋 (QR → 실제 놓을 위치, 실측 필요)
-PLACE_OFFSET_X = -125.0
+PLACE_OFFSET_X = -150.0
 PLACE_OFFSET_Y = 10.0
 PLACE_OFFSET_Z = -40.0
 PLACE_RX = -178.0
